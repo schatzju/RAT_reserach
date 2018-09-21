b12a455f8f9b278b309b44291fb7c1d3b46bff07
schatzju <schatzju@umich.edu>
2018-09-21 14:31:45 -0400

    small fixes

--- run_all.sh
+++ run_all.sh
@@ -1,13 +1,19 @@
 #create folder with date
+OUTPUT_NAME_NO_FAN="output/rat_run_no_fan_"`date +"%d_%m_%Y"`
+OUTPUT_NAME_NO_ASSOC="output/rat_run_no_assoc_"`date +"%d_%m_%Y"`
 OUTPUT_NAME="output/rat_run_"`date +"%d_%m_%Y"`
 
+mkdir $OUTPUT_NAME_NO_FAN
+mkdir $OUTPUT_NAME_NO_ASSOC
 mkdir $OUTPUT_NAME
 
-./a.out c 0 smem_soar_databases/hbc_single_8_26_18 $OUTPUT_NAME
-./a.out f 20 smem_soar_databases/hbc_single_8_26_18 $OUTPUT_NAME
+#./a.out c 0 hbc_single_8_26_18 $OUTPUT_NAME
+./a.out f 4 hbc_single_8_26_18 $OUTPUT_NAME_NO_FAN "smem --set spreading-use-only association"
+./a.out f 4 hbc_single_8_26_18 $OUTPUT_NAME_NO_ASSOC "smem --set spreading-use-only fan"
+./a.out f 4 hbc_single_8_26_18 $OUTPUT_NAME "smem --set spreading-use-only both"
 
-./a.out f 20 smem_soar_databases/hbc_single_subset_8_26_18 $OUTPUT_NAME
-./a.out c 0 smem_soar_databases/hbc_single_subset_8_26_18 $OUTPUT_NAME
+#./a.out f 20 hbc_single_subset_8_26_18 $OUTPUT_NAME
+#./a.out c 0 hbc_single_subset_8_26_18 $OUTPUT_NAME
 
-./a.out f 20 smem_soar_databases/hbc_single_trash_8_26_18 $OUTPUT_NAME
-./a.out c 0 smem_soar_databases/hbc_single_trash_8_26_18 $OUTPUT_NAME
+#./a.out f 20 hbc_single_trash_8_26_18 $OUTPUT_NAME
+#./a.out c 0 hbc_single_trash_8_26_18 $OUTPUT_NAME
