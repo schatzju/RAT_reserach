0e6dd843e28c9fbaaf8166ab971ebf79a9a67ab7
Jule Schatz <schatzju@Soar-Mac-Pro.local>
2018-10-04 11:45:29 -0400

    edits

--- run_all.sh
+++ run_all.sh
@@ -8,12 +8,16 @@ mkdir $OUTPUT_NAME_NO_ASSOC
 mkdir $OUTPUT_NAME
 
 #./a.out c 0 hbc_single_8_26_18 $OUTPUT_NAME
-./a.out f 4 hbc_single_8_26_18 $OUTPUT_NAME_NO_FAN "smem --set spreading-use-only association"
-./a.out f 4 hbc_single_8_26_18 $OUTPUT_NAME_NO_ASSOC "smem --set spreading-use-only fan"
-./a.out f 4 hbc_single_8_26_18 $OUTPUT_NAME "smem --set spreading-use-only both"
+#./a.out f 4 hbc_single_8_26_18 $OUTPUT_NAME_NO_FAN "smem --set spreading-use-only association"
+#./a.out f 4 hbc_single_8_26_18 $OUTPUT_NAME_NO_ASSOC "smem --set spreading-use-only fan"
+#./a.out f 4 hbc_single_8_26_18 $OUTPUT_NAME "smem --set spreading-use-only both"
 
 #./a.out f 20 hbc_single_subset_8_26_18 $OUTPUT_NAME
 #./a.out c 0 hbc_single_subset_8_26_18 $OUTPUT_NAME
+./a.out f 4 hbc_single_trash_8_26_18 $OUTPUT_NAME "smem --set spreading-use-only both"
+./a.out f 4 hbc_single_trash_8_26_18 $OUTPUT_NAME_NO_FAN "smem --set spreading-use-only association"
 
-#./a.out f 20 hbc_single_trash_8_26_18 $OUTPUT_NAME
-#./a.out c 0 hbc_single_trash_8_26_18 $OUTPUT_NAME
+./a.out f 4 hbc_single_8_26_18 $OUTPUT_NAME "smem --set spreading-use-only both"
+./a.out f 4 hbc_single_8_26_18 $OUTPUT_NAME_NO_FAN "smem --set spreading-use-only association"
+#./a.out f 4 hbc_single_trash_8_26_18 $OUTPUT_NAME_NO_ASSOC "smem --set spreading-use-only fan"
+#./a.out f 4 hbc_single_trash_8_26_18 $OUTPUT_NAME "smem --set spreading-use-only both"
